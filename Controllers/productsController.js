9ac54c557373ab6813ff6c2ba6232b2214cd2e5b
Michela <micheladellagatta1@gmail.com>
2026-01-12 18:06:13 +0100

    add image extension to products

--- Controllers/productsController.js
+++ Controllers/productsController.js
@@ -1,14 +1,20 @@
 const connection = require("../database/connection");
 
+const addImageExtension = (product) => {
+  return {
+    ...product,
+    url_image: "http://localhost:3000/" + product.url_image + ".jpeg",
+  };
+};
+
 const index = (req, res) => {
   const sql = "SELECT * FROM products";
   connection.query(sql, (err, resu) => {
     if (err) {
-      return res.status(500).json({
-        error: "il database ha fallito",
-      });
+      return res.status(500).json({ error: "il database ha fallito" });
     }
-    res.json(resu);
+    const prodotti = resu.map(addImageExtension);
+    res.json(prodotti);
   });
 };
 
@@ -16,13 +22,10 @@ const show = (req, res) => {
   const id = req.params.id;
   const sql = "SELECT * FROM products WHERE product_id = ?";
   connection.query(sql, [id], (err, resu) => {
-    if (err)
-      return res.status(500).json({
-        error: "problemi con il server",
-      });
+    if (err) return res.status(500).json({ error: "problemi con il server" });
     if (resu.length === 0)
       return res.status(404).json({ error: "prodotto inesistente" });
-    const prodotto = resu[0];
+    const prodotto = addImageExtension(resu[0]);
     res.json(prodotto);
   });
 };
@@ -38,17 +41,11 @@ const search = (req, res) => {
   `;
 
   connection.query(sql, [`%${term}%`, `%${term}%`], (err, results) => {
-    if (err) {
-      return res.status(500).json({
-        error: "errore nella query di ricerca",
-      });
-    }
-    res.json(results);
+    if (err)
+      return res.status(500).json({ error: "errore nella query di ricerca" });
+    const prodotti = results.map(addImageExtension);
+    res.json(prodotti);
   });
 };
 
-module.exports = {
-  index,
-  show,
-  search,
-};
+module.exports = { index, show, search };
