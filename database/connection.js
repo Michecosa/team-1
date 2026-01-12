ca54d36997c55f78e1c1d7fea412add5516b45a6
giannivitale <giavitale78@gmail.com>
2026-01-12 19:25:52 +0100

    fix: update database connection user variable and add dotenv dependency

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 })
