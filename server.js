a5057371693de65fc89fd72f590362df44738c29
Michela <micheladellagatta1@gmail.com>
2026-01-15 11:30:41 +0100

    Refactor server setup to include error handling middleware and improve code organization

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = 3000;
 const productsRouter = require("./Routers/productsRouter");
 const ordersRouter = require("./Routers/ordersRouter");
 const emailsRouter = require("./Routers/emailsRouter");
+const notFound = require("./middlewares/notFound");
+const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
 
 app.use(
@@ -12,20 +14,23 @@ app.use(
   })
 );
 
-//da controllare
-app.use(express.json({}));
-
-//uso della cartella public
+app.use(express.json());
 app.use(express.static("public"));
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
 app.get("/", (req, res) => {
   res.send("home page");
+});
+
+app.get("/errore", (req, res) => {
+  throw new Error("Errore di test");
 });
 
 app.use("/api/products", productsRouter);
 app.use("/api/emails", emailsRouter);
 app.use("/orders", ordersRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
